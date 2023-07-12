2815e83272d1b69e4fe2dd4f4ae2ee9f0bf9e683
sudip1310 <137476692+sudip1310@users.noreply.github.com>
2023-07-12 12:59:10 +0530

    Update doctor.py

--- doctor.py
+++ doctor.py
@@ -8,7 +8,7 @@ def doctor_page():
     st.write("")
     st.write("")
 
-    static_doctor_content=Image.open("images\Doctor_page_static content.png")
+    static_doctor_content=Image.open("images/Doctor_page_static content.png")
     st.image(static_doctor_content,use_column_width=True)
 
 
